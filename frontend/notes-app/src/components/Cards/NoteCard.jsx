814a6048581c82a394eb87f4b3c6aa23ae43c9b8
Adarsh <157718190+adarshjha305@users.noreply.github.com>
2024-08-10 13:03:09 +0530

    NoteCard, getAllNotes at frontend side working fine

--- frontend/notes-app/src/components/Cards/NoteCard.jsx
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -1,15 +1,26 @@
+import moment from "moment";
 import React from "react";
 import { MdOutlinePushPin } from "react-icons/md";
 import { MdCreate, MdDelete } from "react-icons/md";
 
 const NoteCard = ({
-    title, date, content, tags, isPinned, onEdit, onDelete, onPinNote }) => {
+    title,
+    date,
+    content,
+    tags,
+    isPinned,
+    onEdit,
+    onDelete,
+    onPinNote
+}) => {
     return (
         <div className="border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out">
             <div className="flex items-center justify-between">
                 <div>
                     <h6 className="text-sm font-medium">{title}</h6>
-                    <span className="text-xs text-slate-500"></span>
+                    <span className="text-xs text-slate-500">
+                        {moment(date).format('Do MMM YYYY')}
+                    </span>
                 </div>
 
                 <MdOutlinePushPin
@@ -20,7 +31,7 @@ const NoteCard = ({
             <p className="text-xm text-slate-600 mt-2">{content?.slice(0, 60)}</p>
 
             <div className="flex items-center justify-between mt-2">
-                <div className="text-xs text-slate-500">{tags}</div>
+                <div className="text-xs text-slate-500">{tags.map((item => `#${item} `))}</div>
 
                 <div className="flex items-center gap-2">
                     <MdCreate
